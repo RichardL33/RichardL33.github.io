10b08f65cd7d6db81d4a8ec02cbc143484c1d62c
Richard Lee <richard.lee@digitaslbi.com>
2017-08-05 20:41:40 -0400

    update initMap debug

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-$(function($){
-	function initMaps() {
-		var map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: 40.8054491, lng: -73.9654415
-			}
-		});
-	}
-})
+function initMap() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+	center: {
+		lat: 40.8054491, lng: -73.9654415
+		}
+	});
+}
